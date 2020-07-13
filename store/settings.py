c4adaa66c26435644287c38010a54d30dec9ddf3
RafaAlkhamry <rafaAlkhamry2020>
2020-07-13 06:05:13 -1200

    Update setings file

--- store/settings.py
+++ store/settings.py
@@ -123,7 +123,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
